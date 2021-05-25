528bc8bb53cda3961de6f0c8a4ad3a7fa58bb410
luckmer <woszczo04@gmail.com>
2021-05-25 14:50:46 +0200

    data

---
+++ src/ts/GlobalData.ts
@@ -0,0 +1,12 @@
+export class GlobalData {
+  public static player: number[] = [28, 27, 26];
+  public static currentPosition: number = 1;
+  public static IntervalTime: number = 200;
+  public static randomBlockNumber: number = 65;
+  public static snakeStart: boolean = true;
+  public static CollectedPoints: number = 0;
+  public static width: number = 12;
+  public static counter: number = 0;
+  public static Points: number = 0;
+  public static PlayTimer: any = null;
+}
