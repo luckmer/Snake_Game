9c4d345fbaa48de4f4c07cbe0702b591c913e648
luckmer <woszczo04@gmail.com>
2021-05-25 14:51:22 +0200

    map generator

---
+++ src/ts/MapGenerator.ts
@@ -0,0 +1,52 @@
+import { Game } from "./import";
+
+const grid = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+class MapGenerator {
+  public grid: number[];
+  public game: HTMLDivElement[];
+
+  constructor(grid: number[]) {
+    this.grid = grid;
+    this.game = [];
+  }
+
+  createBoard() {
+    const grid = this.grid;
+    const array = this.game;
+
+    for (let i = 0; i < grid.length; i++) {
+      const div = document.createElement("div");
+      div.id = grid[i].toString();
+      Game?.appendChild(div);
+      array.push(div);
+      if (grid[i] === 1) {
+        array[i].className = "wall";
+      }
+      if (grid[i] === 0) {
+        array[i].className = "space";
+      }
+    }
+  }
+
+  getBoard() {
+    const array = this.game;
+    return array;
+  }
+}
+
+const mapGenerator = new MapGenerator(grid);
+mapGenerator.createBoard();
+
+export default mapGenerator;
